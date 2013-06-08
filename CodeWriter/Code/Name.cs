ac4b6cc9189d9639f574ab0c0c3b43b1c7b6fcc4
Peter Hultqvist <phq@silentorbit.com>
2013-06-08 15:14:11 +0200

    CodeWriter: CamelCase names containing "-" Used by css classes

--- CodeWriter/Code/Name.cs
+++ CodeWriter/Code/Name.cs
@@ -12,19 +12,14 @@ namespace SilentOrbit.Code
 
 			string csname = "";
 
-			if (name.Contains("_") == false)
-				csname += name.Substring(0, 1).ToUpperInvariant() + name.Substring(1);
-			else
+			foreach (string part in name.Split('_', '-'))
 			{
-				foreach (string part in name.Split('_'))
-				{
-					if (part.Length == 0)
-						csname += "_";
-					else
-						csname += part.Substring(0, 1).ToUpperInvariant() + part.Substring(1);
-				}
-			}       
-
+				if (part.Length == 0)
+					csname += "_";
+				else
+					csname += part.Substring(0, 1).ToUpperInvariant() + part.Substring(1);
+			}
+		
 			return csname;          
 		}
 	}
