6b9a62e25df3914b26ecd8203d93e296894d8055
Dmitriy Vornychev <dm.s.vornychev@gmail.com>
2020-06-06 15:32:12 +0300

    Restore ability to use as exe (#52)
    Import CommandLineParser as nuget package

--- CodeGenerator/Program.cs
+++ CodeGenerator/Program.cs
@@ -5,6 +5,20 @@ namespace SilentOrbit.ProtocolBuffers
 {
     public static class Program
     {
+        public static int Main(string[] args)
+        {
+            try
+            {
+                Options.TryParse(args, (options) => Build(options));
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                return -1;
+            }
+        }
+
         public static void Build(Options options)
         {
             var parser = new FileParser();
